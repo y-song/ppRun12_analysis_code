e50ad0fee6953d7b8120df6a2ef5d4c5cad7e19e
youqi <y.song@berkeley.edu>
2023-05-03 15:31:27 -0400

    updates

--- run_matching.sh
+++ run_matching.sh
@@ -2,5 +2,5 @@
 
 #root -l -q 'Test.cxx+("geant0901_allevt.root", "geant0901.root")'
 #root -l -q 'GetMcevi.cxx+("pythia0901.root", "geant0901_allevt.root")'
-root -l -q 'MatchGeantToPythia.cxx+("pythia0901.root", "geant0901_eff096.root", "all0901_eff096.root", "mcevi.list")'
+root -l -q 'MatchGeantToPythia.cxx+("pythia0412.root", "geant0412.root", "all0412.root", "mcevi.list")'
 #root -l -q 'MatchGeantToPythia.cxx+("pythia0901.root", "geant0901_trkcut_evtptcut_jp2.root", "all0901_trkcut_evtptcut_jp2.root", "mcevi.list")'
