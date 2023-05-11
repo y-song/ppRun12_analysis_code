931eeb52d99a6321b1d0e86e704f902f4ce407b6
youqi <y.song@berkeley.edu>
2023-05-11 07:48:09 -0400

    Reset to 0.2 GeV track pT cut

--- container.sh
+++ container.sh
@@ -4,5 +4,5 @@ cd /gpfs01/star/pwg/youqi/run12/ppRun12_analysis_code/
 echo $1
 source setup.sh
 make
-./bin/RunppTestAna -i /gpfs01/star/pwg/youqi/run12/ppRun12_analysis_code/raw_data/$1 -intype pico -c JetTree -trig ppJP2 -o /gpfs01/star/pwg/youqi/run12/ppRun12_analysis_code/Results/data/out_$1 -N -1 -pj 10 2000 -mj 1 -pc 2.0 30 -lja antikt -ec 1 -R 0.4 -geantnum 0 -hadcorr 1 -towunc 0 -fakeeff 1
+./bin/RunppTestAna -i /gpfs01/star/pwg/youqi/run12/ppRun12_analysis_code/raw_data/$1 -intype pico -c JetTree -trig ppJP2 -o /gpfs01/star/pwg/youqi/run12/ppRun12_analysis_code/Results/data/out_$1 -N -1 -pj 10 2000 -mj 1 -pc 0.2 30 -lja antikt -ec 1 -R 0.4 -geantnum 0 -hadcorr 1 -towunc 0 -fakeeff 1
 #./bin/RunppTestAna -i /gpfs01/star/pwg/youqi/run12/ppRun12_analysis_code/$1 -intype pico -c JetTree -trig ppJP2 -o /gpfs01/star/pwg/youqi/run12/ppRun12_analysis_code/Results/data/out_$1 -N -1 -pj 15 2000 -mj 1 -pc 0.2 2000 -lja antikt -ec 1 -R 0.4 -geantnum 0 -hadcorr 1 -towunc 0 -fakeeff 1 
--- run_pythia_analysis.sh
+++ run_pythia_analysis.sh
@@ -2,7 +2,7 @@
 
 for file in `cat geant.list`
 	do
-	./bin/RunppTestAna -i '/gpfs01/star/pwg/elayavalli/ppRun12Embpicos/'$file -intype mcpico -c JetTreeMc -trig all -o 'Results/pythia/Pythia'$file -N -1 -pj 5 2000 -mj 0.0 -pc 2.0 30 -lja antikt -ec 1 -R 0.4 -geantnum 1 -jetnef 1
+	./bin/RunppTestAna -i '/gpfs01/star/pwg/elayavalli/ppRun12Embpicos/'$file -intype mcpico -c JetTreeMc -trig all -o 'Results/pythia/Pythia'$file -N -1 -pj 5 2000 -mj 0.0 -pc 0.2 30 -lja antikt -ec 1 -R 0.4 -geantnum 1 -jetnef 1
 	#./bin/RunppTestAna -i '/gpfs01/star/pwg/elayavalli/ppRun12Embpicos/'$file -intype mcpico -c JetTreeMc -trig all -o 'Results/pythia/Pythia'$file -N -1 -pj 0.2 2000 -mj 0.0 -pc 0.2 1000 -lja antikt -ec 1 -R 0.4 -geantnum 1 -jetnef 1
 	
 done
